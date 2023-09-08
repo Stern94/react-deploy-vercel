9d67d5c24e1fe34f33d02e2ce57e7ec95d5b7d97
Stern94 <128252264+Stern94@users.noreply.github.com>
2023-09-07 22:31:44 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './App.css';
+import App from './App';
+<meta httpEquiv='Content Security Policy'  content="upgrade-insecure-requests"/>
+
+ReactDOM.render(<App />, document.getElementById('root'));
