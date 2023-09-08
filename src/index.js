49f7ee57878cc56ee5659c0f518c83710f868c60
Stern94 <128252264+Stern94@users.noreply.github.com>
2023-09-07 20:05:19 -0700

    Delete src/index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import './App.css';
-import App from './App';
-<meta httpEquiv='Content Security Policy'  content="upgrade-insecure-requests"/>
-
-ReactDOM.render(<App />, document.getElementById('root'));
